1065d85faadcec33e6675ba0201879c1b23343ae
Kevin Tyll <kevintyll@gmail.com>
2010-09-02 14:42:46 -0400

    change from import to load to eliminate jruby conflict

--- lib/ofac.rb
+++ lib/ofac.rb
@@ -6,4 +6,4 @@ require 'ofac/models/ofac_sdn_loader'
 require 'ofac/models/ofac'
 
 # Load rake file
-import "#{File.dirname(__FILE__)}/tasks/ofac.rake"
+load "#{File.dirname(__FILE__)}/tasks/ofac.rake"
